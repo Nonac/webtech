6872b196c6cc9e7e3d46348b21f5d9b6d62171f7
Nonac <nonacyang@gmail.com>
2020-05-15 09:24:24 +0800

    add

---
+++ src/register.js
@@ -0,0 +1,38 @@
+// eslint-disable-next-line no-undef
+new Vue({
+    el: '#register',
+    data(){
+        return{
+            userName: "",
+            password: "",
+            rePassword:"",
+            showMyUsername: null,
+            showMyPassword: null,
+            showDisplay: false,
+            invaildLebalShow:false,
+            rePasswordLegal:false
+        }
+    },
+    methods: {
+        save:function(){
+            this.rePasswordLegal = this.rePassword === this.password;
+
+            if(this.rePasswordLegal) {
+                this.showMyUsername = this.userName;
+                this.showMyPassword = this.password;
+                this.invaildLebalShow=false;
+                this.userName = "";
+                this.password = "";
+                this.rePassword = "";
+                this.showDisplay = true;
+            }else{
+                this.invaildLebalShow=true;
+                this.userName = "";
+                this.password = "";
+                this.rePassword = "";
+                this.showDisplay = true;
+            }
+        }
+    }
+
+});
