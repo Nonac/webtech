3ddd1d41dac39d89d5772fc9d3b8c169cea71d27
Nonac <nonacyang@gmail.com>
2020-06-06 09:00:14 +0800

    report

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#"  @click="homeClick">Make Your Own Resume</a>
+        <a class="navbar-brand" href="#"  @click="homeClick">Simple Resume Maker</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
