c0b66e7d2df86b582b03b125f43b03fa82438703
bojo <wzxutao@gmail.com>
2020-06-07 18:58:52 +0100

    href of self-evaluation to github/../report.pdf

--- src/components/selfEvaluation.vue
+++ src/components/selfEvaluation.vue
@@ -1,85 +1,103 @@
 <template>
-    <section class="row-section">
-        <div class="row">
-            <h2 class="text-center">
-                <span>Self-Evaluation</span>
-            </h2>
-        </div>
-        <div class="col-md-10 offset-md-1 row-block">
-            <ul id="sortable">
-                <li><div class="media">
-                    <div class="media-body">
-                        <h4>HTML</h4>
-                        <p>
-                            content
-                        </p>
-                    </div>
-                </div></li>
-                <li><div class="media">
-                    <div class="media-body">
-                        <h4>CSS</h4>
-                        <p>
-                            content
-                        </p>
-                    </div>
-                </div></li>
-                <li><div class="media">
-                    <div class="media-body">
-                        <h4>JS</h4>
-                        <p>
-                            content
-                        </p>
-                    </div>
-                </div></li>
-                <li><div class="media">
-                    <div class="media-body">
-                        <h4>PNG</h4>
-                        <p>
-                            content
-                        </p>
-                    </div>
-                </div></li>
-                <li><div class="media">
-                    <div class="media-body">
-                        <h4>SVG</h4>
-                        <p>
-                            content
-                        </p>
-                    </div>
-                </div></li>
-                <li><div class="media">
-                    <div class="media-body">
-                        <h4>Server</h4>
-                        <p>
-                            content
-                        </p>
-                    </div>
-                </div></li>
-                <li><div class="media">
-                    <div class="media-body">
-                        <h4>Database</h4>
-                        <p>
-                            content
-                        </p>
-                    </div>
-                </div></li>
-                <li><div class="media">
-                    <div class="media-body">
-                        <h4>Dynamic pages</h4>
-                        <p>
-                            content
-                        </p>
-                    </div>
-                </div></li>
-            </ul>
-        </div>
-    </section>
+<section class="row-section">
+  <a href="https://github.com/Nonac/webtech/blob/master/Report/report.pdf">
+    <div class="row">
+      <h2 class="text-center">
+        <span>Self-Evaluation</span>
+      </h2>
+    </div>
+    <div class="col-md-10 offset-md-1 row-block">
+      <ul id="sortable">
+        <li>
+          <div class="media">
+            <div class="media-body">
+              <h4>HTML</h4>
+              <p>
+                content
+              </p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="media">
+            <div class="media-body">
+              <h4>CSS</h4>
+              <p>
+                content
+              </p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="media">
+            <div class="media-body">
+              <h4>JS</h4>
+              <p>
+                content
+              </p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="media">
+            <div class="media-body">
+              <h4>PNG</h4>
+              <p>
+                content
+              </p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="media">
+            <div class="media-body">
+              <h4>SVG</h4>
+              <p>
+                content
+              </p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="media">
+            <div class="media-body">
+              <h4>Server</h4>
+              <p>
+                content
+              </p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="media">
+            <div class="media-body">
+              <h4>Database</h4>
+              <p>
+                content
+              </p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="media">
+            <div class="media-body">
+              <h4>Dynamic pages</h4>
+              <p>
+                content
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </a>
+</section>
 </template>
 
 <script>
-    export default {
-        name: "selfEvaluation"
-    }
+export default {
+  name: "selfEvaluation"
+}
 </script>
 
 <style scoped src="../view/index/assets/about.css">
